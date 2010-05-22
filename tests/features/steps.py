98ee602bd01e18923a6b55b4910b6dba5d36b843
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2010-05-22 10:34:10 -0300

    Take off the global variables of the steps to use the global storage areas from frashen module

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -1,21 +1,20 @@
 from freshen import *
 
 from player import Player
-from buraco import Buraco
+from buraco.buraco import Buraco
 
 @Given(r'I have (\d+) players, with names (.*)')
 def given_i_have_a_player_named(number, names):
     names = names.split(', ')
-    global players; players = []
+    scc.players = []
     for name in names:
-        players.append(Player(name))
+        scc.players.append(Player(name))
 
 @When(r'I initialize the game')
 def when_i_initialize_the_game():
-    global buraco
-    buraco = Buraco(players)
+    scc.buraco = Buraco(scc.players)
 
 @Then(r'the cards are distributed')
 def then_each_player_has_11_cards():
-    buraco.distribute_cards()
+    scc.buraco.distribute_cards()
 
